5e96cd9d796aa9f66cc76cafcaf994555d9e0ccb
John Paul Fababaer <100891759+jpfababaer@users.noreply.github.com>
2024-02-12 21:01:57 +0000

    updated notes

--- app/controllers/departments_controller.rb
+++ app/controllers/departments_controller.rb
@@ -16,13 +16,13 @@ class DepartmentsController < ApplicationController
     @department = Department.new
     @department.name = params.fetch("query_name")
 
-  #Q1 How does this method "create" connect back to the .erb files index.html.erb
+  #Q1 How does this method "create" connect back to the .erb files index.html.erb - There is some background logic ("syntactic sugar") that Rails offers. For now, just need to know this works.
 
-  #Q2 The attribute "action" with insert_department, is that just a placeholder URL? So when the form is actually submitted, the "redirect_to" points the new page back to /departments instead of /insert_department?
+  #Q2 The attribute "action" with insert_department, is that just a placeholder URL? So when the form is actually submitted, the "redirect_to" points the new page back to /departments instead of /insert_department? Yes, this points back to the main route follows that logic.
 
   #Q3 What does the symbol :notice do or refer to? When I place a valid department name, I don't see the message "Department created successfully" so is the second argument necessary (same for when it fails)?
 
-  #Q4 Best place to read documentation for ActiveRecord methods, etc. 
+  #Q4 Best place to read documentation for ActiveRecord methods, etc. -> RailsonRubyguides
 
   #NOTES -> SEARCH app/views/courses/index
     if @department.valid?
